6e12b0f9ce150f98b6a4c634fec6ca40afd06e67
quannt <simpleplan.vn@gmail.com>
2015-12-21 00:52:33 +0800

    use x-ray to craw a sample page

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var Xray = require('x-ray');
 var x = Xray();
 
+var url = "http://data.nowgoal.com/1x2/bet007history.htm?id=&company=&matchdate=";
+var date = "2015-12-13";
+var fileName = date.replace(/-/g,'_') + '.json';
 
-x('http://google.com', 'title')(function(err, title) {
-  console.log(title) // Google
+var selector =  "tr";
+
+x(url + date, 'tr', [['td']])(function(err,arr){
+	if (err){
+		console.log("Error occured for " + date, err);
+	}
+	console.log(arr);
 })
+.write(fileName);
