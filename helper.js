fbae653ede0927a3ee5294b08e5852bd8e1ae3aa
quannt <simpleplan.vn@gmail.com>
2016-01-06 23:32:21 +0800

    sleep a random amount of time

--- helper.js
+++ helper.js
@@ -1,8 +1,13 @@
-var sleep = function(time){
+var sleep = function(){
+	var sleepTime = getRandomIntInclusive(3000, 5000);
 	setTimeout(function(){
-		console.log("Sleeping for " + time + " ms");
-	},time);
+		console.log("Sleeping for " + sleepTime + " ms");
+	},sleepTime);
 };
 
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 exports.sleep = sleep;
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var Xray = require('x-ray');
 var reader = require('./reader');
 var moment = require('moment');
 var helper = require("./helper");
-var Batch = require('batch')
-  , batch = new Batch;
+var Excel = require("exceljs");
 
 
 var x = Xray();
@@ -12,14 +11,15 @@ var x = Xray();
 var url = "http://data.nowgoal.com/1x2/bet007history.htm?id=&company=&matchdate=";
 
 
-var startDate= moment("2014-11-24");
-var endDate = moment("2015-12-26");
-var usingDate = moment("2014-11-25");
+var startDate= moment("2014-12-31");
+var endDate = moment("2016-01-01");
+var usingDate = moment("2015-01-01");
 
 
 var fileName = "./data/" + endDate.format("YYYY_MM_DD") + '.json';
 
 var nowGoalWorkbook = reader.createNowGoalOutputTemplate();
+
 
 var rowProcessedCallback = function(err,arr){
 	if (err){
@@ -29,12 +29,16 @@ var rowProcessedCallback = function(err,arr){
 };
 
 
-while(usingDate.isBetween(startDate, endDate)){
+
+	while(usingDate.isBetween(startDate, endDate)){
 	console.log("Adding " + usingDate.format("YYYY/MM/DD") + " to the q");
-	helper.sleep(2000);
+	helper.sleep();
 	x(url + usingDate.format("YYYY-MM-DD"), 'tr', [['td']])(rowProcessedCallback) ;
 	usingDate.add(1,'days');
-};
+	};
+
+
+
 
 
 
